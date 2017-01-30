bcd228361eb0f7d848a4c108aaab9cb886483a38
Jessica Younker <jessica.younker@gmail.com>
2017-01-30 09:27:34 -0600

    finished tree

--- main.js
+++ main.js
@@ -1,38 +1,76 @@
-var tree = {
+var pineTree = {
 	height: 0,
-	character: "",
+	leaf: 0
 };
 
-function treeMaker(){
-}
+pineTree.height = document.getElementById("height");
+pineTree.leaf = document.getElementById("leaf");
 
-var heightInput = document.getElementById("height");
-var characterInput = document.getElementById("leaf");
 var growTree = document.getElementById("growButton");
 var outputTree = document.getElementById("output-target");
 
-heightInput.addEventListener("keypress", pressed);
-characterInput.addEventListener("keypress", pressed);
-growTree.addEventListener("click", treeMaker);
+pineTree.height.addEventListener("keypress", pressed);
+pineTree.leaf.addEventListener("keypress", pressed);
+growTree.addEventListener("click", inputTest);
 
 function pressed(enterPress) {
-	if (enterPress.keyCode === 13){
+	if (enterPress.keyCode === 13) {
 		treeMaker();
-	}
-}
+	}};
 
-// var charArray = [];
-// 	tree.character = characterInput.value;
-// 	tree.height = heightInput.value;
-// 		charArray.push(tree.character * (tree.height ^2));
-// 			return (charArray);
-// 			console.log(charArray);
-
+function inputTest() {
+	if (pineTree.height.value === "") {
+		alert("Enter height");
+		return false;
+		console.log("height alert")
 	
-// 			for (var i = 0, i < charArray.length, i++);
-// 				return (charArray
+	} else if (pineTree.leaf.value === "") {
+		alert("Select a character");
+		return false;
 
-//get total number of characters (via row-squared) then do line breaks and centering after every two characters?
+	} else {
+		treeMaker()
+	}
+};
+
+function treeMaker(){
+	var spaceCalc = pineTree.height.value - 1;
+	var charCalc = 1;
+
+	for (i=0; i< pineTree.height.value; i++ ) {
 
 
+	var spaces = " ".repeat(spaceCalc);
+	var chars = pineTree.leaf.value.repeat(charCalc);
+	var treeGrow = spaces + chars;
+	console.log(treeGrow);
+
+	spaceCalc -= 1;
+	charCalc += 2;
+	};
+
+};
+
+//this function displays a tree in the inner.HTML output 
+//(but only a righttriangle in console)
+
+	// var grow;
+	// var leafCalc = pineTree.leaf.value;
+	// var htCalc = (2 * (pineTree.height.value) - 1);
+
+	// for (i = 0; i < pineTree.height.value; i++ ) {
+	// 	// console.log("height", pineTree.height.value);
+	// 	// console.log("leaf",pineTree.leaf.value);
+	// 	grow = pineTree.leaf.value.repeat(i + 1);
+	// 	console.log(grow);
+		
+	// 	outputTree.innerHTML += `<center><p>${grow}</p></center>`;		
+	// 	// for (i = 0; i < grow.length; i++) {
+	// 	// 	2(i)-1 + "<br>";
+	// 	// }
+	// }
+
+// }
+	
+	
 
